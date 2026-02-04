87d5c48f75a3d8c3f188b1665a5dcc6f76e5459f
Michele-Brignola <michele.brignola@hotmail.com>
2026-02-04 15:38:15 +0100

    added imgs and header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,22 @@
+import LogoDc from "../assets/img/dc-logo.png";
+import "../css/header.css";
+
 export default function Header() {
   return (
-    <header>
-      <h1>Sono l'Header</h1>
+    <header id="header" className="container d-flex justify-content-between">
+      <img className="py-3" src={LogoDc} alt="Logo"></img>
+      <nav className="d-flex align-items-center">
+        <a href="#">Characters</a>
+        <a href="#">Comics</a>
+        <a href="#">Movies</a>
+        <a href="#">TV</a>
+        <a href="#">Games</a>
+        <a href="#">Collectables</a>
+        <a href="#">Videos</a>
+        <a href="#">Fans</a>
+        <a href="#">News</a>
+        <a href="#">Shop</a>
+      </nav>
     </header>
   );
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,8 @@
+import "../css/index.css";
+
 export default function Main() {
   return (
-    <main>
+    <main id="main">
       <h1>Sono il Main</h1>
     </main>
   );
